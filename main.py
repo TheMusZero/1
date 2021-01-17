6ac51bf60bba520e9c05ba73e5396bf595eba7cc
user <Fedorovvladislav030@gmail.com>
2021-01-17 14:01:01 +0300

    2

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 import pygame
+from game_config import config
+from GameMain import GameMain
 
-from Enemy import Enemy
-from Game import Game
-from Hero import Hero
-from labirint import Labirint, WINDOW_SIZE, ENEMY_EVENT_TYPE, FPS
+if __name__ == '__main__':
+    level = input('Введите название уровня')
 
-
-def main():
     pygame.init()
-    screen = pygame.display.set_mode(WINDOW_SIZE)
-
-    labirint = Labirint("simple_map.txt", [0, 2], 2)
-    hero = Hero((7, 7))
-    enemy = Enemy((7, 1))
-    game = Game(labirint, hero, enemy)
+    pygame.display.set_caption('')
+    width, height = config.get_list_values('width', 'height')
+    size = width, height
+    screen = pygame.display.set_mode(size)
+    config.set_value('screen', screen)
+    config.set_value('level', level)
     clock = pygame.time.Clock()
     running = True
+    game = GameMain()
+    fps = 60
+    dt = 0
     while running:
         for event in pygame.event.get():
+            game.on_event(event)
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == ENEMY_EVENT_TYPE:
-                game.move_enemy()
-        game.update_hero()
-        screen.fill((0, 0, 0))
-        game.render(screen)
+        game.draw(dt)
+        game.update()
         pygame.display.flip()
-        clock.tick(FPS)
+        dt = clock.tick(fps)
     pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
