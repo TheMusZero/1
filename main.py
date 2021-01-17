bcd86a0362c4cd37f8fd1edc45d6508512a71280
user <Fedorovvladislav030@gmail.com>
2021-01-17 13:59:49 +0300

    2

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import pygame
-from game_config import config
-from AssetManager import assetManager
-from Grass import GrassGroup
-from Box import BoxGroup
-from Player import PlayerGroup
-from Camera import camera
+
+from Enemy import Enemy
+from Game import Game
+from Hero import Hero
+from labirint import Labirint, WINDOW_SIZE, ENEMY_EVENT_TYPE, FPS
 
 
-class LevelScreen:
-    def __init__(self):
-        self.screen = config.get_value('screen')
-        level = self.load_level(config.get_value('level'))
-        level = [list(line) for line in level]
-        self.groups = {'grass': GrassGroup(level), 'box': BoxGroup(level), 'player': PlayerGroup(level)}
-        self.groups['player'].set_walls(self.groups['box'])
-        camera.update(self.groups['player'].player)
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
 
-    def draw(self, dt=None):
-        self.screen.fill(pygame.Color('black'))
-        camera.apply_group(*self.groups.values())
-        for group_name in self.groups:
-            self.groups[group_name].draw(self.screen)
+    labirint = Labirint("simple_map.txt", [0, 2], 2)
+    hero = Hero((7, 7))
+    enemy = Enemy((7, 1))
+    game = Game(labirint, hero, enemy)
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == ENEMY_EVENT_TYPE:
+                game.move_enemy()
+        game.update_hero()
+        screen.fill((0, 0, 0))
+        game.render(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
 
-    def on_event(self, event):
-        if event.type == pygame.KEYUP:
-            self.groups['player'].on_key_pressed(event.key)
 
-    def load_level(self, level):
-        return assetManager.load_level(f'levels/{level}.txt')
-
-    def update(self):
-        for group_name in self.groups:
-            self.groups[group_name].update()
+if __name__ == "__main__":
+    main()
